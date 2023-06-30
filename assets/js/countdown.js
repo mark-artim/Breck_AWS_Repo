46978e2f895d6b0b7f87e488b21394ef54008709
Mark Artim <mark.artim@gmail.com>
2023-06-30 10:02:56 -0500

    trying to get countdown to work

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -18,6 +18,7 @@ var x = setInterval(function() {
   // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
+  document.getElementById("days").innerHTML = days;
 
   // If the count down is finished, write some text
   if (distance < 0) {
